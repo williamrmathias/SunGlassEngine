8ec21de658a2872c256d34fd0e57e5a56b5bcdee
William Mathias <williamrmathias45@gmail.com>
2023-07-04 11:50:11 -0700

    Added color attribute to shaders

--- FirstApp.cpp
+++ FirstApp.cpp
@@ -29,9 +29,9 @@ namespace LittleVulkanEngine {
 
 	void FirstApp::loadModels() {
 		std::vector<LveModel::Vertex> vertices{
-			{{0.0f, -0.5f}},
-			{{0.5f, 0.5f}},
-			{{-0.5f, 0.5f}}
+			{{0.0f, -0.5f}, {1.0f, 0.0f, 0.0f}},
+			{{0.5f, 0.5f}, {0.0f, 1.0f, 0.0f}},
+			{{-0.5f, 0.5f}, {0.0f, 0.0f, 1.0f}}
 		};
 
 		lveModel = std::make_unique<LveModel>(lveDevice, vertices);
--- LittleVulkanEngineModel.hpp
+++ LittleVulkanEngineModel.hpp
@@ -19,6 +19,7 @@ namespace LittleVulkanEngine {
 
 		struct Vertex {
 			glm::vec2 position;
+			glm::vec3 color;
 
 			static std::vector<VkVertexInputBindingDescription> getBindingDescriptions();
 			static std::vector<VkVertexInputAttributeDescription> getAttributeDescriptions();
